9405478b362cbb3adc0a012d8af2a7a024bd6001
Minuka Weerathunga <minuka.weerathunga@itelasoft.com>
2021-11-20 07:57:07 +0530

    [FE] Created Signup Page

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,36 +1,93 @@
 import React, { Component } from "react";
+import {
+  Card,
+  Button,
+  Form,
+  Col,
+  Row,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
+import styles from "./Signup.css";
 
 export default class SignUp extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign Up</h3>
+  state = {
+    data: {
+      name: null,
+      email: null,
+      password: null,
+    },
+  };
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
-                </p>
-            </form>
-        );
-    }
+  onSubmit = () => {
+    console.log(this.state.data)
+  }
+  render() {
+    return (
+      <Card className="text-center">
+        <Card.Header>Sign Up</Card.Header>
+        <div className="centered-div">
+          <Card.Body>
+            <Form className="form-content">
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Full Name</InputGroup.Text>
+                <FormControl
+                  aria-label="Full Name"
+                  placeholder="John Doe"
+                  type="text"
+                  value={this.state.data.name}
+                  onChange={e => this.setState(prevState => ({
+                    data: {
+                      ...prevState.data,
+                      name: e.target.value 
+                    }
+                  }))}
+                />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Email</InputGroup.Text>
+                <FormControl
+                  aria-label="Email"
+                  placeholder="name@example.com"
+                  type="email"
+                  value={this.state.data.email}
+                  onChange={e => this.setState(prevState => ({
+                    data: {
+                      ...prevState.data,
+                      email: e.target.value 
+                    }
+                  }))}
+                />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Password</InputGroup.Text>
+                <FormControl
+                  aria-label="password"
+                  placeholder="********"
+                  type="password"
+                  value={this.state.data.password}
+                  onChange={e => this.setState(prevState => ({
+                    data: {
+                      ...prevState.data,
+                      password: e.target.value 
+                    }
+                  }))}
+                />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <Button
+                  className="btnFormSend"
+                  variant="outline-success"
+                  onClick={this.onSubmit}
+                >
+                  Send Feedback
+                </Button>
+              </InputGroup>
+            </Form>
+          </Card.Body>
+        </div>
+        <Card.Footer className="text-muted">Movie App</Card.Footer>
+      </Card>
+    );
+  }
 }
