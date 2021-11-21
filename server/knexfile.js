6139ade416adbccd9c0a19f6a62685932db7178f
Minuka Weerathunga <minuka.weerathunga@itelasoft.com>
2021-11-21 19:20:15 +0530

    [BE] Added Database Migrations and migration support

---
+++ server/knexfile.js
@@ -0,0 +1,42 @@
+const dbConfig = require('./dbConfig');
+const connection = dbConfig.config;
+
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
