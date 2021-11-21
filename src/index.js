ea0b58cad86504c1f32522c12ec101682390a6bb
Minuka Weerathunga <minuka.weerathunga@itelasoft.com>
2021-11-21 19:23:42 +0530

    [FE] downgraded Router dom package and changed routing methods

--- src/index.js
+++ src/index.js
@@ -1,37 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import rootReducer from "./reducers";
-import App from "./App";
 import "./index.css";
 import Wishlist from "./routes/Wishlist";
 import Movie from "./routes/Movie";
 import Home from "./routes/Home";
 import Profile from "./routes/Profile";
-import SignIn from "./containers/Signin"
-import SignUp from "./containers/Signup"
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import SignIn from "./containers/Signin";
+import SignUp from "./containers/Signup";
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="movie" element={<Movie/>} />
-        <Route path="wishlist" element={<Wishlist/>} />
-        <Route path="profile" element={<Profile/>} />
-        <Route path="signin" element={<SignIn/>} />
-        <Route path="signup" element={<SignUp/>} />
-      </Routes>
-    </BrowserRouter>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/movie" exact component={Movie} />
+            <Route path="/wishlist" exact component={Wishlist} />
+            <Route path="/profile" exact component={Profile} />
+            <Route path="/signin" exact component={SignIn} />
+            <Route path="/signup" exact component={SignUp} />
+          </Switch>
+        </div>
+      </Router>
   </Provider>,
   document.getElementById("root")
 );
