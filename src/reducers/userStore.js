430af22af4d039ada90784cf975747d87fdeaf05
Minuka Weerathunga <minuka.weerathunga@itelasoft.com>
2021-11-23 21:51:18 +0530

    [FE] Added User login and logout

--- src/reducers/userStore.js
+++ src/reducers/userStore.js
@@ -1,52 +1,33 @@
 const userState = {
-    user: {},
-    isLoggedIn: false,
-    error: null
-}
+  user: {},
+  isLoggedIn: false,
+  error: null,
+};
 
 const userStore = (state = userState, action) => {
-    switch (action.type) {
-        case "SIGNIN":
-            if (action.success) {
-                state.isLoggedIn = true;
-                state.user = action.data;
-                state.error = null;
-            }
-            console.log(state);
-            return state;
-        case "SIGNOUT":
-            if (action.success) {
-                state.isLoggedIn = false;
-                state.user = null;
-                state.error = null;
-            }
-            return state;
-        case "ERROR":
-            state.isLoggedIn = false;
-            state.user = null;
-            return state;
-        default:
-            return state;
-    }
-}
-
-// const userReducer = async (state = state, action) => {
-//     switch (action.type) {
-//         case "SIGNIN":
-//             const [user, isLoggedIn] = await login(action.payload);
-//             if(isLoggedIn) {
-//                 state.isLoggedIn = isLoggedIn;
-//                 state.user = user;
-//             }
-//                 return state;
-//         case "SIGNOUT":
-//             state.isLoggedIn = false;
-//             state.user = null;
-//             return state;
-//         default:
-//             return state;
-//     }
-// };
-//
+  switch (action.type) {
+    case "SIGNIN":
+      if (action.success) {
+        state.isLoggedIn = true;
+        state.user = action.data;
+        state.error = null;
+      }
+      console.log(state);
+      return state;
+    case "SIGNOUT":
+      if (action.success) {
+        state.isLoggedIn = false;
+        state.user = null;
+        state.error = null;
+      }
+      return state;
+    case "ERROR":
+      state.isLoggedIn = false;
+      state.user = null;
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default userStore;
