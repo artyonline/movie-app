893ce91c89e30b161de410ee371df644bc695e22
Minuka Weerathunga <minuka.weerathunga@itelasoft.com>
2021-12-01 17:51:22 +0530

    [FE] Added logout route

---
+++ src/routes/Logout.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+export default function Profile() {
+  import { useDispatch } from "react-redux";
+  return <div onClick={() => ({})}></div>;
+}
