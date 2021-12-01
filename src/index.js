b12c96dc5e5fd3306553ab5e7f20d7141a845a8d
Minuka Weerathunga <minuka.weerathunga@itelasoft.com>
2021-12-01 17:51:59 +0530

    [FE] Lint fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
 import Wishlist from "./routes/Wishlist";
@@ -15,18 +15,18 @@ import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/movie" exact component={Movie} />
-            <Route path="/wishlist" exact component={Wishlist} />
-            <Route path="/profile" exact component={Profile} />
-            <Route path="/signin" exact component={SignIn} />
-            <Route path="/signup" exact component={SignUp} />
-          </Switch>
-        </div>
-      </Router>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie" exact component={Movie} />
+          <Route path="/wishlist" exact component={Wishlist} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/signin" exact component={SignIn} />
+          <Route path="/signup" exact component={SignUp} />
+        </Switch>
+      </div>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
